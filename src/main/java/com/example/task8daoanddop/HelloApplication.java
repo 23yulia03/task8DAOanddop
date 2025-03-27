dae3cc08847ab934ec6b5fe21f01524015ca2c37
23yulia03 <kozlovskayayulia23@gmail.com>
2025-03-28 00:37:13 +0300

    Программа работает!

--- src/main/java/com/example/task8daoanddop/HelloApplication.java
+++ src/main/java/com/example/task8daoanddop/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 540);
-        stage.setTitle("Task 8ADAO");
+        stage.setTitle("Task 8A-DAO");
         stage.setScene(scene);
         stage.show();
     }

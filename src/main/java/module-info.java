62236af73a370334a0628a5a7d4173288de0e5c9
23yulia03 <kozlovskayayulia23@gmail.com>
2025-04-04 01:27:28 +0300

    Commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.task8daoanddop {
     requires org.apache.poi.poi;
     requires org.apache.poi.ooxml;
     requires java.sql;
+    requires io.github.cdimascio.dotenv.java;
 
 
     opens com.example.task8daoanddop to javafx.fxml;
